222e1b9e5d354c8d35ae3f882344563bfcf76b34
EdsonRego <edsonxrego@gmail.com>
2025-10-12 10:28:51 -0300

    Dev profile fixed

--- backend/src/main/java/com/edsonrego/taskmanager/config/SecurityConfig.java
+++ backend/src/main/java/com/edsonrego/taskmanager/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.edsonrego.taskmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Profile({"prod", "test"})
 public class SecurityConfig {
 
     @Bean
